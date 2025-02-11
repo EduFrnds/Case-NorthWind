3e715bef16a76a522665a5c2756601deef5e990e
Eduardo <edufernandes.contato@gmail.com>
2025-02-11 15:57:06 -0300

    commit_06 - Criação dos KPIs, sales e atualização do schema_analytics.yml

--- northwind_dbt/models/analytics/sales/kpi_avg_order_value.sql
+++ northwind_dbt/models/analytics/sales/kpi_avg_order_value.sql
@@ -1,5 +1,7 @@
-{{ config(materialized='view', schema='data_analytics') }}
-
+{{ config(
+    materialized='view',
+    schema='data_analytics'
+) }}
 
 WITH order_details AS (
     SELECT
@@ -11,18 +13,22 @@ WITH order_details AS (
 
 orders AS (
     SELECT
-        order_id
+        order_id,
+        customer_id
     FROM {{ ref('stg_orders') }}
 ),
 
-calculated_values AS (
+customer_revenue AS (
     SELECT
+        o.customer_id,
         SUM(od.unit_price * od.quantity) AS total_revenue,
         COUNT(DISTINCT o.order_id) AS total_orders
     FROM order_details od
     JOIN orders o ON od.order_id = o.order_id
+    GROUP BY o.customer_id
 )
 
 SELECT
-    total_revenue / NULLIF(total_orders, 0) AS average_order_value
-FROM calculated_values
+    customer_id,
+    total_revenue / NULLIF(total_orders, 0) AS avg_order_value_per_customer
+FROM customer_revenue
